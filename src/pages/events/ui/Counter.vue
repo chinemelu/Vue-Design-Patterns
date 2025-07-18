682db908076b3e7256e27b099eb2e566581d43a5
Chinemelu Nwosu <chinemelunwosu@gmail.com>
2025-07-18 23:51:22 +0100

    Add emit declaration with stricter object syntax

--- src/pages/events/ui/Counter.vue
+++ src/pages/events/ui/Counter.vue
@@ -1,14 +1,31 @@
 <template>
-    <button role="increment" @click="count += 1" />
-    <button role="submit" @click="$emit('submit' , count)" />
+    <button role="increment" @click="increment" />
+    <button role="submit" @click="submit" />
 </template>
 
 <script lang="ts">
     export default {
         name: "AppCounter",
+        // emits: ['submit'],
+        emits: {
+            submit: (count: number) => {
+                // another way of declaring emits
+                // if the validation fails, there will be no emission
+                // If the validator returns false, the event will not be emitted.
+                return typeof count !== 'string' && !isNaN(count)
+            }
+        },
         data() {
             return {
                 count: 0
+            }
+        },
+        methods: {
+            increment() {
+                this.count += 1
+            },
+            submit() {
+                this.$emit('submit' , this.count)
             }
         }
     }
